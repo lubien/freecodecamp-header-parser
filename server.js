41aae998e7343e017ce50a1f5bc9d5183797e211
lubien <lubien1996@gmail.com>
2016-02-25 23:52:16 -0300

    Add @awalthefirst solution to get IP

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 var app = require('express')();
-var get_ip = require('ipware')().get_ip;
 
 app.route('/').get(function (req, res) {
   res.send({
-    ipaddress: get_ip(req)['clientIp'] || false,
+    ipaddress: req.headers['x-forwarded-for'] || false,
     language: req.headers['accept-language'].split(',')[0] || false,
     software: /\((.*?)\)/.exec(req.headers['user-agent'])[1] || false
   });
