965aa8715bc0770ee7d5ab258cfb62fcd3174242
lubien <lubien1996@gmail.com>
2016-02-25 23:37:01 -0300

    Use Node IPware to get user's IP real address

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 var app = require('express')();
+var get_ip = require('ipware')().get_ip;
 
 app.route('/').get(function (req, res) {
   res.send({
-    ipaddress: req.ip || false,
+    ipaddress: get_ip(req)['clientIp'] || false,
     language: req.headers['accept-language'].split(',')[0] || false,
     software: /\((.*?)\)/.exec(req.headers['user-agent'])[1] || false
   });
