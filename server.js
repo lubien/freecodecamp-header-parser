334b16a493705a3a5d2d6b2ec7e70a7349754e1a
lubien <lubien1996@gmail.com>
2016-02-25 23:24:30 -0300

    Initial

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+var app = require('express')();
+
+app.route('/').get(function (req, res) {
+  res.send({
+    ipaddress: req.ip || false,
+    language: req.headers['accept-language'].split(',')[0] || false,
+    software: /\((.*?)\)/.exec(req.headers['user-agent'])[1] || false
+  });
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Started service at port', port);
+});
